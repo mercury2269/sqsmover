8de932b7833df87ee2134334f0feb392b43c42c2
Sergey Maskalik <mercury2269@gmail.com>
2018-11-15 17:44:08 -0800

    Add error logging around queue names

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/mercury2269/sqsmover
 require (
 	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
 	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/apex/log v1.1.0
 	github.com/aws/aws-sdk-go v1.15.76
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/tj/go-progress v0.0.0-20180508172012-fadc638a53dd // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
--- main.go
+++ main.go
@@ -2,30 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/cli"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/fatih/color"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
 	sourceQueue      = kingpin.Flag("source", "Source queue to move messages from").Short('s').Required().String()
 	destinationQueue = kingpin.Flag("destination", "Destination queue to move messages to").Short('d').Required().String()
-	region			= kingpin.Flag("region", "AWS Region for source and destination queues").Short('r').Default("us-west-2").String()
+	region           = kingpin.Flag("region", "AWS Region for source and destination queues").Short('r').Default("us-west-2").String()
 )
 
-
-
-func resolveQueueUrl(queueName string, svc *sqs.SQS) (error, string) {
+func resolveQueueUrl(svc *sqs.SQS, queueName string) (error, string) {
 	params := &sqs.GetQueueUrlInput{
 		QueueName: aws.String(queueName),
 	}
 	resp, err := svc.GetQueueUrl(params)
 
 	if err != nil {
-		// Print the error, cast err to awserr.Error to get the Code and
-		// Message from an error.
-		fmt.Println(err.Error())
 		return err, ""
 	}
 
@@ -33,26 +31,80 @@ func resolveQueueUrl(queueName string, svc *sqs.SQS) (error, string) {
 }
 
 func main() {
-	kingpin.UsageTemplate(kingpin.CompactUsageTemplate)
+	log.SetHandler(cli.Default)
 
+	fmt.Println()
+	defer fmt.Println()
+
+	kingpin.UsageTemplate(kingpin.CompactUsageTemplate)
 	kingpin.Parse()
 
-	svc := sqs.New(session.New(), aws.NewConfig().WithRegion(*region))
-
-	err, sourceUrl := resolveQueueUrl(*sourceQueue, svc)
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(*region)})
 
 	if err != nil {
+		log.Error(color.New(color.FgRed).Sprintf("Unable to create AWS session for region: ", *region))
 		return
 	}
 
-	err, destUrl := resolveQueueUrl(*destinationQueue, svc)
+	svc := sqs.New(sess)
+
+	err, sourceQueueUrl := resolveQueueUrl(svc, *sourceQueue)
 
 	if err != nil {
+		log.Error(color.New(color.FgRed).Sprintf("Unable to locate the source queue with name: %s, check region and name", *sourceQueue))
 		return
 	}
 
+	log.Info(color.New(color.FgCyan).Sprintf("Source queue url: %s", sourceQueueUrl))
+
+	err, destinationQueueUrl := resolveQueueUrl(svc, *destinationQueue)
+
+	if err != nil {
+		log.Error(color.New(color.FgRed).Sprintf("Unable to locate the destination queue with name: %s, check region and name", *sourceQueue))
+		return
+	}
+
+	log.Info(color.New(color.FgCyan).Sprintf("Destination queue url: %s", destinationQueueUrl))
+
+	queueAttributes, err := svc.GetQueueAttributes(&sqs.GetQueueAttributesInput{
+		QueueUrl:       aws.String(sourceQueueUrl),
+		AttributeNames: []*string{aws.String("All")},
+	})
+
+	log.Info(color.New(color.FgCyan).Sprintf("Approximate number of messages in the source queue: %s",
+		*queueAttributes.Attributes["ApproximateNumberOfMessages"]))
+
+	moveMessages(sourceQueueUrl, destinationQueueUrl, svc)
+
+}
+
+func convertToEntries(messages []*sqs.Message) []*sqs.SendMessageBatchRequestEntry {
+	result := make([]*sqs.SendMessageBatchRequestEntry, len(messages))
+	for i, message := range messages {
+		result[i] = &sqs.SendMessageBatchRequestEntry{
+			MessageBody: message.Body,
+			Id:          message.MessageId,
+		}
+	}
+
+	return result
+}
+
+func convertSuccessfulMessageToBatchRequestEntry(messages []*sqs.Message) []*sqs.DeleteMessageBatchRequestEntry {
+	result := make([]*sqs.DeleteMessageBatchRequestEntry, len(messages))
+	for i, message := range messages {
+		result[i] = &sqs.DeleteMessageBatchRequestEntry{
+			ReceiptHandle: message.ReceiptHandle,
+			Id:            message.MessageId,
+		}
+	}
+
+	return result
+}
+
+func moveMessages(sourceQueueUrl string, destinationQueueUrl string, svc *sqs.SQS) {
 	params := &sqs.ReceiveMessageInput{
-		QueueUrl:            aws.String(sourceUrl), // Required
+		QueueUrl:            aws.String(sourceQueueUrl),
 		VisibilityTimeout:   aws.Int64(1),
 		WaitTimeSeconds:     aws.Int64(1),
 		MaxNumberOfMessages: aws.Int64(10),
@@ -79,7 +131,7 @@ func main() {
 		fmt.Println(resp.Messages)
 
 		batch := &sqs.SendMessageBatchInput{
-			QueueUrl: aws.String(destUrl),
+			QueueUrl: aws.String(destinationQueueUrl),
 			Entries:  convertToEntries(resp.Messages),
 		}
 
@@ -103,7 +155,7 @@ func main() {
 		if len(sendResp.Successful) == len(resp.Messages) {
 			deleteMessageBatch := &sqs.DeleteMessageBatchInput{
 				Entries:  convertSuccessfulMessageToBatchRequestEntry(resp.Messages),
-				QueueUrl: aws.String(sourceUrl),
+				QueueUrl: aws.String(sourceQueueUrl),
 			}
 
 			deleteResp, err := svc.DeleteMessageBatch(deleteMessageBatch)
@@ -123,29 +175,4 @@ func main() {
 			fmt.Println("========================")
 		}
 	}
-
-}
-
-func convertToEntries(messages []*sqs.Message) []*sqs.SendMessageBatchRequestEntry {
-	result := make([]*sqs.SendMessageBatchRequestEntry, len(messages))
-	for i, message := range messages {
-		result[i] = &sqs.SendMessageBatchRequestEntry{
-			MessageBody: message.Body,
-			Id:          message.MessageId,
-		}
-	}
-
-	return result
-}
-
-func convertSuccessfulMessageToBatchRequestEntry(messages []*sqs.Message) []*sqs.DeleteMessageBatchRequestEntry {
-	result := make([]*sqs.DeleteMessageBatchRequestEntry, len(messages))
-	for i, message := range messages {
-		result[i] = &sqs.DeleteMessageBatchRequestEntry{
-			ReceiptHandle: message.ReceiptHandle,
-			Id:            message.MessageId,
-		}
-	}
-
-	return result
 }
