dea670a38aae1a5ffeefd33d5d462c555e607299
Brandon Burnett <gentoolicious@gmail.com>
2022-06-14 15:21:44 -0700

    Fix tab spacing.

--- main.go
+++ main.go
@@ -242,24 +242,24 @@ func moveMessages(sourceQueueUrl string, destinationQueueUrl string, svc *sqs.SQ
 		}
 
 		if len(sendResp.Successful) == len(messagesToCopy) {
-      if *deleteFromSource {
-        deleteMessageBatch := &sqs.DeleteMessageBatchInput{
-          Entries:  convertSuccessfulMessageToBatchRequestEntry(messagesToCopy),
-          QueueUrl: aws.String(sourceQueueUrl),
-        }
+			if *deleteFromSource {
+				deleteMessageBatch := &sqs.DeleteMessageBatchInput{
+					Entries:  convertSuccessfulMessageToBatchRequestEntry(messagesToCopy),
+					QueueUrl: aws.String(sourceQueueUrl),
+				}
 
-        deleteResp, err := svc.DeleteMessageBatch(deleteMessageBatch)
+				deleteResp, err := svc.DeleteMessageBatch(deleteMessageBatch)
 
-        if err != nil {
-          logAwsError("Failed to delete messages from source queue", err)
-          return
-        }
+				if err != nil {
+					logAwsError("Failed to delete messages from source queue", err)
+					return
+				}
 
-        if len(deleteResp.Failed) > 0 {
-          log.Error(color.New(color.FgRed).Sprintf("Error deleting messages, the following were not deleted\n %s", deleteResp.Failed))
-          return
-        }
-      }
+				if len(deleteResp.Failed) > 0 {
+					log.Error(color.New(color.FgRed).Sprintf("Error deleting messages, the following were not deleted\n %s", deleteResp.Failed))
+					return
+				}
+			}
 
 			messagesProcessed += len(messagesToCopy)
 		}
